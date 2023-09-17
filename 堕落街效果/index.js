18763664716cb367154b7886c106c6cb9443d415
rewriting45 <rewriting45@163.com>
2023-09-17 11:14:04 +0800

    堕落街效果

---
+++ 堕落街效果/index.js
@@ -0,0 +1,8 @@
+let text = document.querySelectorAll("ul li a").forEach(text => {
+    text.innerHTML = text.innerText.split("").map((letters, i) => `<span>${letters}</span>`).join("");
+    let span = document.querySelectorAll("ul li a span").forEach(e => {
+        let duration = Math.random() * 5;
+        e.style.animationDuration = 0.25 + duration + "s";
+        e.style.animationDelay = 0.25 + duration + "s";
+    })
+})
